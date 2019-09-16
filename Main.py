d3f6717ca7392489c24860c60644a7315d3fa181
Morton <caigmort@iu.edu>
2019-09-16 18:36:39 -0400

    No Readme

--- Main.py
+++ Main.py
@@ -1,8 +1,19 @@
-birthday_dictionary = {
-	"Courtney Young": June 26,
-	"Sarah Lamb": November 23,
-	"Ilsa Shaikh": ,
-	"Kevin Morton": june 17
-}
 
->>>birthday_dictionary["Courtney Young"] = June 26
+if __name__ == '__main__':
+	
+	birthdays = {
+		'Courtney Young': 'June 26',
+		'Sarah Lamb': 'November 23',
+		'Ilsa Shaikh': 'March 18',
+		'Kevin Morton': 'June 17' }
+		
+	print('This is my tiny library of my friends\' birthdays! The birthdays we have on record are:')
+	for name in birthdays:
+		print (name)
+
+	print('Who\'s bithday would you like to know?')
+	name = input()
+	if name in birthdays:
+		print('{}\'s birthday is {}.' .format(name, birthdays[name]))
+	else:
+		print('Sorry, but that person is not in my Library.'.format (name))
