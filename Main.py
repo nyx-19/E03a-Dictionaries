1d4fc00de8202a4de0c0d71d46c7aa809526cf7b
nyx-19 <54558770+nyx-19@users.noreply.github.com>
2019-09-16 17:20:55 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+birthday_dictionary = {
+	"Courtney Young": June 26,
+	"Sarah Lamb": November 23,
+	"Ilsa Shaikh": ,
+	"Kevin Morton": june 17
+}
+
+>>>birthday_dictionary["Courtney Young"] = June 26
